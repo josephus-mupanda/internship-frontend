546c93debd1ad229beb8d7f2d22677fd3b6dd021
josephus <josephusmupanda48@gmail.com>
2024-09-03 16:31:28 +0200

    add new urls in app_config

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'environment.dart';
 
 class AppConfig {
@@ -7,5 +9,13 @@ class AppConfig {
   static const String groupUrl = '${Environment.baseUrl}/api/groups';
 
   static const String memberUrl = '${Environment.baseUrl}/api/members';
+
+  static const String contributionUrl = '${Environment.baseUrl}/api/contributions';
+
+  static const String disbursementUrl = '${Environment.baseUrl}/api/disbursements';
+
+  static const String transactionUrl = '${Environment.baseUrl}/api/transactions';
+
+  static const String loanUrl = '${Environment.baseUrl}/api/loans';
 
 }
