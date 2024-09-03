bc506f795e834ed1a9049279395d1be1a8d198b1
josephus <josephusmupanda48@gmail.com>
2024-09-03 17:51:51 +0200

    add disbursement service

---
+++ lib/data/services/disbursement_service.dart
@@ -0,0 +1,70 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../models/disbursement.dart';
+import '../../core/config/app_config.dart';
+
+class DisbursementService {
+  final String baseUrl = AppConfig.disbursementUrl;
+
+  // Get all disbursements
+  Future<http.Response> getAllDisbursements(String token) async {
+    final response = await http.get(
+      Uri.parse(baseUrl),
+      headers: {
+        'Authorization': 'Bearer $token',
+        'Content-Type': 'application/json',
+      },
+    );
+    return response;
+  }
+
+  // Get disbursement by ID
+  Future<http.Response> getDisbursementById(String id, String token) async {
+    final response = await http.get(
+      Uri.parse('$baseUrl/$id'),
+      headers: {
+        'Authorization': 'Bearer $token',
+        'Content-Type': 'application/json',
+      },
+    );
+    return response;
+  }
+
+  // Create a new disbursement
+  Future<http.Response> createDisbursement(Disbursement disbursement, String token) async {
+    final response = await http.post(
+      Uri.parse(baseUrl),
+      headers: {
+        'Authorization': 'Bearer $token',
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode(disbursement.toJson()),
+    );
+    return response;
+  }
+
+  // Update an existing disbursement
+  Future<http.Response> updateDisbursement(String id, Disbursement disbursement, String token) async {
+    final response = await http.put(
+      Uri.parse('$baseUrl/$id'),
+      headers: {
+        'Authorization': 'Bearer $token',
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode(disbursement.toJson()),
+    );
+    return response;
+  }
+
+  // Delete a disbursement
+  Future<http.Response> deleteDisbursement(String id, String token) async {
+    final response = await http.delete(
+      Uri.parse('$baseUrl/$id'),
+      headers: {
+        'Authorization': 'Bearer $token',
+        'Content-Type': 'application/json',
+      },
+    );
+    return response;
+  }
+}
