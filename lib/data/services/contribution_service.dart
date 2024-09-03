6cc46cfe2fa888eb2d44313514c062b9bf27e9b0
josephus <josephusmupanda48@gmail.com>
2024-09-03 17:49:33 +0200

    add contribution service

---
+++ lib/data/services/contribution_service.dart
@@ -0,0 +1,70 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../models/contribution.dart';
+import '../../core/config/app_config.dart';
+
+class ContributionService {
+  final String baseUrl = AppConfig.contributionUrl;
+
+  // Get all contributions
+  Future<http.Response> getAllContributions(String token) async {
+    final response = await http.get(
+      Uri.parse(baseUrl),
+      headers: {
+        'Authorization': 'Bearer $token',
+        'Content-Type': 'application/json',
+      },
+    );
+    return response;
+  }
+
+  // Get contribution by ID
+  Future<http.Response> getContributionById(String id, String token) async {
+    final response = await http.get(
+      Uri.parse('$baseUrl/$id'),
+      headers: {
+        'Authorization': 'Bearer $token',
+        'Content-Type': 'application/json',
+      },
+    );
+    return response;
+  }
+
+  // Create a new contribution
+  Future<http.Response> createContribution(Contribution contribution, String token) async {
+    final response = await http.post(
+      Uri.parse(baseUrl),
+      headers: {
+        'Authorization': 'Bearer $token',
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode(contribution.toJson()),
+    );
+    return response;
+  }
+
+  // Update an existing contribution
+  Future<http.Response> updateContribution(String id, Contribution contribution, String token) async {
+    final response = await http.put(
+      Uri.parse('$baseUrl/$id'),
+      headers: {
+        'Authorization': 'Bearer $token',
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode(contribution.toJson()),
+    );
+    return response;
+  }
+
+  // Delete a contribution
+  Future<http.Response> deleteContribution(String id, String token) async {
+    final response = await http.delete(
+      Uri.parse('$baseUrl/$id'),
+      headers: {
+        'Authorization': 'Bearer $token',
+        'Content-Type': 'application/json',
+      },
+    );
+    return response;
+  }
+}
