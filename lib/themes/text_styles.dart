8222ff5374710b428214b0483096e22389d8c7e6
josephus <josephusmupanda48@gmail.com>
2024-09-04 10:40:40 +0200

    add text styles

---
+++ lib/themes/text_styles.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class TextStyles {
+  static const TextStyle headline1 = TextStyle(
+    fontSize: 32.0,
+    fontWeight: FontWeight.bold,
+    color: Colors.black,
+  );
+
+  static const TextStyle headline2 = TextStyle(
+    fontSize: 24.0,
+    fontWeight: FontWeight.bold,
+    color: Colors.black,
+  );
+
+  static const TextStyle bodyText1 = TextStyle(
+    fontSize: 16.0,
+    color: Colors.black,
+  );
+
+  static const TextStyle bodyText2 = TextStyle(
+    fontSize: 14.0,
+    color: Colors.black,
+  );
+
+// Define other text styles as needed
+}
