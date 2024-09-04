a57694bbfbd092b8f4ab376d9c1cf9b398ec65f9
josephus <josephusmupanda48@gmail.com>
2024-09-04 11:55:19 +0200

    add loading and toast utils

---
+++ lib/core/utils/loading.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void showLoadingDialog(BuildContext context) {
+  showDialog(
+    context: context,
+    barrierDismissible: false,
+    builder: (BuildContext context) {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    },
+  );
+}
