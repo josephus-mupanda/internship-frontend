7b6976a64d66eee7e938ccbd2e99e7fca01dbc0c
josephus <josephusmupanda48@gmail.com>
2024-09-04 10:57:37 +0200

    add app theme

---
+++ lib/themes/app_theme.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'color_palette.dart';
+import 'text_styles.dart';
+
+class AppTheme {
+
+  static final ThemeData lightTheme = ThemeData(
+    primaryColor: ColorPalette.primaryColor,
+    primaryColorDark: ColorPalette.primaryVariant,
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: ColorPalette.primaryColor,
+      primary: ColorPalette.primaryColor,
+      onPrimary: ColorPalette.onPrimaryColor,
+      secondary: ColorPalette.secondaryColor,
+      onSecondary: ColorPalette.onSecondaryColor,
+      background: ColorPalette.backgroundColor,
+      onBackground: ColorPalette.onBackgroundColor,
+      error: ColorPalette.errorColor,
+      onError: ColorPalette.onErrorColor,
+    ),
+    textTheme: const TextTheme(
+      headlineLarge: TextStyles.headline1,
+      headlineMedium: TextStyles.headline2,
+      bodyLarge: TextStyles.bodyText1,
+      bodyMedium: TextStyles.bodyText2,
+    ),
+    buttonTheme: const ButtonThemeData(
+      buttonColor: ColorPalette.primaryColor,
+      textTheme: ButtonTextTheme.primary,
+    ),
+    appBarTheme: const AppBarTheme(
+      backgroundColor: ColorPalette.primaryColor,
+      titleTextStyle: TextStyle(
+        color: ColorPalette.onPrimaryColor,
+        fontSize: 20.0,
+      ),
+    ),
+  );
+
+  static final ThemeData darkTheme = ThemeData(
+    primaryColor: ColorPalette.primaryColor,
+    primaryColorDark: ColorPalette.primaryVariant,
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: ColorPalette.primaryColor,
+      primary: ColorPalette.primaryColor,
+      onPrimary: ColorPalette.onPrimaryColor,
+      secondary: ColorPalette.secondaryColor,
+      onSecondary: ColorPalette.onSecondaryColor,
+      background: ColorPalette.onBackgroundColor, // Use onBackgroundColor for dark
+      onBackground: ColorPalette.backgroundColor, // And backgroundColor for onBackground
+      error: ColorPalette.errorColor,
+      onError: ColorPalette.onErrorColor,
+      brightness: Brightness.dark, // Explicitly set brightness for dark theme
+    ),
+    textTheme: TextTheme(
+      headlineLarge: TextStyles.headline1.copyWith(color: ColorPalette.backgroundColor),
+      headlineMedium: TextStyles.headline2.copyWith(color: ColorPalette.backgroundColor),
+      bodyLarge: TextStyles.bodyText1.copyWith(color: ColorPalette.backgroundColor),
+      bodyMedium: TextStyles.bodyText2.copyWith(color: ColorPalette.backgroundColor),
+    ),
+    buttonTheme: const ButtonThemeData(
+      buttonColor: ColorPalette.primaryColor,
+      textTheme: ButtonTextTheme.primary,
+    ),
+    appBarTheme: const AppBarTheme(
+      backgroundColor: ColorPalette.primaryColor,
+      titleTextStyle: TextStyle(
+        color: ColorPalette.onPrimaryColor,
+        fontSize: 20.0,
+      ),
+    ),
+  );
+}
