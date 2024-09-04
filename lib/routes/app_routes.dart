539c4ad985bbc0f6faee8c428d715a8ef77529e6
josephus <josephusmupanda48@gmail.com>
2024-09-04 10:38:02 +0200

    add app routes

---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,9 @@
+class AppRoutes {
+  static const String splash = '/';
+  static const String onboarding = '/onboarding';
+  static const String login = '/login';
+  static const String register = '/register';
+  static const String resetPassword = '/reset-password';
+  static const String dashboard = '/dashboard';
+// Add more routes as needed
+}
