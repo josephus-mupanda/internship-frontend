a2df3eead17220aed371d8783083a117f1a0d503
josephus <josephusmupanda48@gmail.com>
2024-09-12 12:55:13 +0200

    Update my main.dart for handling incoming urls

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,51 @@
+import 'package:app_links/app_links.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:internship_frontend/routes/app_routes.dart';
 import 'package:internship_frontend/routes/route_generator.dart';
 import 'package:internship_frontend/themes/app_theme.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
+  // Handle incoming links
+  handleIncomingLinks();
+}
+
+void handleIncomingLinks() async {
+  final appLinks = AppLinks();
+
+  // Handle the initial link (if any)
+  final Uri? initialLink = await appLinks.getInitialLink();
+  if (initialLink != null) {
+    _handleIncomingUrl(initialLink);
+  }
+
+  // Handle future incoming links when the app is in the foreground
+  appLinks.uriLinkStream.listen((Uri? uri) {
+    if (uri != null && uri.hasQuery) {
+      _handleIncomingUrl(uri);
+    }
+  });
+}
+
+void _handleIncomingUrl(Uri uri) {
+  final token = uri.queryParameters['token'];
+  if (token != null) {
+    // Ensure navigatorKey is defined in your app for navigation
+    if (navigatorKey.currentContext != null) {
+      Navigator.pushReplacementNamed(
+          navigatorKey.currentContext!,
+          AppRoutes.changePassword,
+          arguments: token
+      );
+    }
+    else {
+      if (kDebugMode) {
+        print('No token found in URL');
+      }
+    }
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -18,6 +59,10 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       initialRoute: AppRoutes.splash,
       onGenerateRoute: RouteGenerator.generateRoute,
+      navigatorKey: navigatorKey, // Use the global navigatorKey
     );
   }
 }
+
+// Define a global navigator key
+final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
