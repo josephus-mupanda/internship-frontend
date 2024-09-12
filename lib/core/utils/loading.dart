52b88b62c26d1ab7c33b6dfcbd978e40f0fea940
josephus <josephusmupanda48@gmail.com>
2024-09-12 10:46:01 +0200

    Modify my loading

--- lib/core/utils/loading.dart
+++ lib/core/utils/loading.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 
+import '../constants/constants.dart';
+
 void showLoadingDialog(BuildContext context) {
   showDialog(
     context: context,
     barrierDismissible: false,
     builder: (BuildContext context) {
-      return const Center(
-        child: CircularProgressIndicator(
-          //color: Theme.of(context).colorScheme.secondary, // Use secondary color from theme
-          //backgroundColor: Theme.of(context).colorScheme.background, // Use background color from theme
+      return const AlertDialog(
+        content: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            CircularProgressIndicator(),
+            SizedBox(width: Constants.kDefaultPadding),
+            Text("Loading..."),
+          ],
         ),
       );
     },
