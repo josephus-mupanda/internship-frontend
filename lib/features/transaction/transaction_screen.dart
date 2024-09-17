3604dc301d13899e386156d96d2451bb960b52ca
josephus <josephusmupanda48@gmail.com>
2024-09-17 16:21:50 +0200

    add transaction screen

---
+++ lib/features/transaction/transaction_screen.dart
@@ -0,0 +1,85 @@
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:websafe_svg/websafe_svg.dart';
+
+import '../../components/side_menu.dart';
+import '../../constants.dart';
+import '../../responsive.dart';
+
+class TransactionScreen extends StatefulWidget {
+  const TransactionScreen({super.key});
+
+  @override
+  State<TransactionScreen> createState() => _TransactionScreenState();
+}
+
+class _TransactionScreenState extends State<TransactionScreen> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _scaffoldKey,
+      drawer: ConstrainedBox(
+        constraints: const BoxConstraints(maxWidth: 250),
+        child: const SideMenu(),
+      ),
+      body: Container(
+        padding: const EdgeInsets.only(top: kIsWeb ? kDefaultPadding : 0),
+        color: kBgDarkColor,
+        child: SafeArea(
+          right: false,
+          child: Column(
+            children: [
+              // This is our Search bar
+              Padding(
+                padding:
+                const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                child: Row(
+                  children: [
+                    // Once user click the menu icon the menu shows like drawer
+                    // Also we want to hide this menu icon on desktop
+                    if (!Responsive.isDesktop(context))
+                      IconButton(
+                        icon: const Icon(Icons.menu),
+                        onPressed: () {
+                          _scaffoldKey.currentState?.openDrawer();
+                        },
+                      ),
+                    if (!Responsive.isDesktop(context)) const SizedBox(width: 5),
+                    Expanded(
+                      child: TextField(
+                        onChanged: (value) {},
+                        decoration: InputDecoration(
+                          hintText: "Transaction",
+                          fillColor: kBgLightColor,
+                          filled: true,
+                          suffixIcon: Padding(
+                            padding: const EdgeInsets.all(
+                                kDefaultPadding * 0.75), //15
+                            child: WebsafeSvg.asset(
+                              "assets/Icons/Search.svg",
+                              width: 24,
+                            ),
+                          ),
+                          border: const OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                            borderSide: BorderSide.none,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: kDefaultPadding),
+
+            ],
+          ),
+        ),
+      ),
+
+    );
+  }
+}
